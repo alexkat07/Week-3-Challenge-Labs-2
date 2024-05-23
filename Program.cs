7011e8f32e1bccc075cd5372f93aa9d7b3191a3f
alexk <alexk@DESKTOP-QL7PNT4>
2024-05-23 15:13:33 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Week_3_Challenge_Labs_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter some letters and numbers");
+
+            string myAnswer = Console.ReadLine();
+
+            Console.WriteLine(SumDigitsInString(myAnswer));
+
+
+
+            static int SumDigitsInString(string userInput) 
+            {
+                int sum = 0;
+                int character;
+                for (int i = 0; i<userInput.Length; i++)
+                {
+                    if (int.TryParse(Convert.ToString(userInput[i]), out character)) 
+                    {
+                        sum += character;
+                    }
+
+                }
+                return sum;
+            }
+           
+            
+        }    
+    }
+}
